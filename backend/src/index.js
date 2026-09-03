80190ee09656887fc5e38ef411d3a920fc8c296c
divyanshjaluthria <35101797+divyanshjaluthria@users.noreply.github.com>
2026-09-04 02:22:12 +0530

    Refactor index.js: clean up commented code, improve CORS configuration, and set default PORT

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,65 @@
-// const express = require("express");
+// // const express = require("express");
+// import express from "express";
+// import cors from "cors";
+// import "dotenv/config";
+// import fs from "fs";
+// import path from "path";
+// import User from "./models/user.model.js";
+// import { connectDB } from "./lib/db.js";
+// import { clerkMiddleware } from "@clerk/express";
+
+// // env data
+// const app = express();
+// const PORT = process.env.PORT;
+// const FRONTEND_URL = process.env.FRONTEND_URL;
+// const publicDir = path.join(process.cwd(), "public");
+
+// //middlewares
+// app.use(express.json());
+// app.use(cors({ orgin: FRONTEND_URL, credentials: true }));
+// app.use(clerkMiddleware());
+
+// // health check
+// app.get("/health", (req, res) => {
+//   res.status(200).json({ ok: true });
+// });
+
+// app.use(express.static(publicDir));
+// app.get("/{*any}", (req, res, next) => {
+//   res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+// });
+// app.listen(PORT, () => {
+//   connectDB();
+//   console.log(`server is up and running in port ${PORT}`);
+// });
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import fs from "fs";
-import path from "path";
+
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from "@clerk/express";
 
-// env data
 const app = express();
-const PORT = process.env.PORT;
-const FRONTEND_URL = process.env.FRONTEND_URL;
-const publicDir = path.join(process.cwd(), "public");
 
-//middlewares
+const PORT = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
 app.use(express.json());
-app.use(cors({ orgin: FRONTEND_URL, credentials: true }));
+
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
 app.use(clerkMiddleware());
 
-// health check
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
 
-app.use(express.static(publicDir));
-app.get("/{*any}", (req, res, next) => {
-  res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
-});
 app.listen(PORT, () => {
   connectDB();
   console.log(`server is up and running in port ${PORT}`);
