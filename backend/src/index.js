0eac345e739790582dcb167e20fc857c4c74648e
divyanshjaluthria <35101797+divyanshjaluthria@users.noreply.github.com>
2026-09-04 01:38:33 +0530

    Add dockerFile and .dockerignore: implement Express with static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,17 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import fs from "fs";
+import path from "path";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from "@clerk/express";
+
 // env data
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+const publicDir = path.join(process.cwd(), "public");
 
 //middlewares
 app.use(express.json());
@@ -18,6 +22,11 @@ app.use(clerkMiddleware());
 // health check
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
+});
+
+app.use(express.static(publicDir));
+app.get("/{*any}", (req, res, next) => {
+  res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
 });
 app.listen(PORT, () => {
   connectDB();
