8d84e644ac0787de9359816ca32ce8d8089ba125
divyanshjaluthria <35101797+divyanshjaluthria@users.noreply.github.com>
2026-09-08 19:12:22 +0530

    Add job import for cron job scheduling

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import fs from "fs";
 import path from "path";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 import { clerkMiddleware } from "@clerk/express";
 
 // env data
