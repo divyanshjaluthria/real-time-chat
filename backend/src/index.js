4d954010e3ad05812e31318f37579410852f1804
divyanshjaluthria <35101797+divyanshjaluthria@users.noreply.github.com>
2026-09-08 17:30:30 +0530

    Refactor code structure for improved readability and maintainability

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,55 +1,57 @@
-// // // const express = require("express");
-// // import express from "express";
-// // import cors from "cors";
-// // import "dotenv/config";
-// // import fs from "fs";
-// // import path from "path";
-// // import User from "./models/user.model.js";
-// // import { connectDB } from "./lib/db.js";
-// // import { clerkMiddleware } from "@clerk/express";
+// const express = require("express");
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import fs from "fs";
+import path from "path";
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
-// // // env data
-// // const app = express();
-// // const PORT = process.env.PORT;
-// // const FRONTEND_URL = process.env.FRONTEND_URL;
-// // const publicDir = path.join(process.cwd(), "public");
+// env data
+const app = express();
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+const publicDir = path.join(process.cwd(), "public");
 
-// // //middlewares
-// // app.use(express.json());
-// // app.use(cors({ orgin: FRONTEND_URL, credentials: true }));
-// // app.use(clerkMiddleware());
+//middlewares
+app.use(express.json());
+app.use(cors({ orgin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware());
 
-// // // health check
-// // app.get("/health", (req, res) => {
-// //   res.status(200).json({ ok: true });
-// // });
+// health check
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
+});
 
-// // app.use(express.static(publicDir));
-// // app.get("/{*any}", (req, res, next) => {
-// //   res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
-// // });
-// // app.listen(PORT, () => {
-// //   connectDB();
-// //   console.log(`server is up and running in port ${PORT}`);
-// // });
+app.use(express.static(publicDir));
+app.get("/{*any}", (req, res, next) => {
+  res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+});
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`server is up and running in port ${PORT}`);
+});
+
 // import express from "express";
 // import cors from "cors";
 // import "dotenv/config";
+// import path from "path";
 
-// import User from "./models/user.model.js";
 // import { connectDB } from "./lib/db.js";
 // import { clerkMiddleware } from "@clerk/express";
 
 // const app = express();
 
 // const PORT = process.env.PORT || 5000;
-// const FRONTEND_URL = process.env.FRONTEND_URL;
+
+// const frontendDist = path.resolve(process.cwd(), "public");
 
 // app.use(express.json());
 
 // app.use(
 //   cors({
-//     origin: FRONTEND_URL,
+//     origin: true,
 //     credentials: true,
 //   }),
 // );
@@ -60,48 +62,16 @@
 //   res.status(200).json({ ok: true });
 // });
 
-// app.listen(PORT, () => {
-//   connectDB();
-//   console.log(`server is up and running in port ${PORT}`);
+// // Serve React frontend
+// app.use(express.static(frontendDist));
+
+// app.get("/*any", (req, res) => {
+//   res.sendFile(path.join(frontendDist, "index.html"));
 // });
 
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import path from "path";
+// app.listen(PORT, () => {
+//   connectDB();
+//   console.log(`server is up and running on port ${PORT}`);
 
-import { connectDB } from "./lib/db.js";
-import { clerkMiddleware } from "@clerk/express";
-
-const app = express();
-
-const PORT = process.env.PORT || 5000;
-
-const frontendDist = path.resolve(process.cwd(), "public");
-
-app.use(express.json());
-
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  }),
-);
-
-app.use(clerkMiddleware());
-
-app.get("/health", (req, res) => {
-  res.status(200).json({ ok: true });
-});
-
-// Serve React frontend
-app.use(express.static(frontendDist));
-
-app.get("/*any", (req, res) => {
-  res.sendFile(path.join(frontendDist, "index.html"));
-});
-
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`server is up and running on port ${PORT}`);
-});
+//   job.start();
+// });
